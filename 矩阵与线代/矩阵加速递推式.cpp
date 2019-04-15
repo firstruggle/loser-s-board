b502decd304e8ca689cf7d88f727eb4eb1d40b22
firstruggle <34533968+firstruggle@users.noreply.github.com>
2019-04-15 09:32:36 +0800

    Update 矩阵加速递推式.cpp

--- 矩阵与线代/矩阵加速递推式.cpp
+++ 矩阵与线代/矩阵加速递推式.cpp
@@ -1,32 +1,29 @@
 //常规矩阵幂
 struct Matrix {
-	int n;
-	int v[maxn][maxn];
-
-	Matrix(int n) { memset(v, 0, sizeof v); this->n = n; }
-
-	friend Matrix operator * (Matrix A, Matrix B) {
-		int n = A.n;
-		Matrix ret(n);
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++)
-				for (int k = 0; k < n; k++) {
-					ret.v[i][j] = ((ll)ret.v[i][j] + (ll)A.v[i][k] * B.v[k][j] % (p - 1)) % (p - 1);
-				}
-		return ret;
-	}
-
-	friend Matrix operator ^ (Matrix A, int k) {
-		int n = A.n;
-		Matrix ret(n);
-		for (int i = 0; i < n; i++)
-			ret.v[i][i] = 1;
-		for (; k; k >>= 1) {
-			if (k & 1)	ret = ret * A;
-			A = A * A;
-		}
-		return ret;
-	}
+    int v[maxn][maxn], n;
+    
+    Matrix(int n) { memset(v, 0, sizeof v); this->n = n; }
+    
+    Matrix operator * (const Matrix &A) const {
+        Matrix ret(n);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                for (int k = 0; k < n; k++)
+                    ret.v[i][j] = ((ll)ret.v[i][j] + (ll)v[i][k] * A.v[k][j] % mod) % mod;
+        return ret;
+    }
+    
+    Matrix operator ^ (int b) const {
+        Matrix ret(n);
+        Matrix A = *this;
+        for (int i = 0; i < n; i++)
+            ret.v[i][i] = 1;
+        for (; b; b >>= 1) {
+            if (b & 1)    ret = ret * A;
+            A = A * A;
+        }
+        return ret;
+    }
 };
 
 
