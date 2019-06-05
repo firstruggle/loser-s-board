f407893a4547fe0944fefc3bef3fd6d095381395
firstruggle <34533968+firstruggle@users.noreply.github.com>
2019-06-05 17:34:18 +0800

    Update z-algorithm.cpp

--- 数据结构/字符串/z-algorithm.cpp
+++ 数据结构/字符串/z-algorithm.cpp
@@ -17,3 +17,22 @@ void Z_algorithm(char *s, int n) {//s从0开始
 		}
 	}
 }
+
+void Z_algorithm(char *s, int n) {//s从1开始
+	int l = 1, r = 1;
+	for (int i = 2; i <= n; i++) {
+		if (i > r) {
+			l = i, r = i;
+			while (r <= n && s[r - l + 1] == s[r])	r++;
+			z[i] = r - l; r--;
+		} else {
+			int k = i - l + 1;
+			if (z[k] < r - i + 1)	z[i] = z[k];
+			else {
+				l = i;
+				while (r <= n && s[r - l + 1] == s[r])	r++;
+				z[i] = r - l; r--;
+			}
+		}
+	}
+}
