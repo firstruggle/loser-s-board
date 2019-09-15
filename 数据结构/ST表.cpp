f042fe92f101bcb8934130291b61605d8a39c4b3
firstruggle <34533968+firstruggle@users.noreply.github.com>
2019-09-16 01:14:27 +0800

    Create ST表.cpp

---
+++ 数据结构/ST表.cpp
@@ -0,0 +1,14 @@
+int rmq(int i, int j) {
+	if (i > j) swap(i, j);
+	int k = 0;
+  while (i + (1 << (k + 1)) <= j) k++;
+  return min(st[i][k], st[j - (1 << k) + 1][k]);
+}
+
+void get_ST() {
+  for (int i = 1; i <= n; i++)
+			st[i][0] = a[i];
+		for (int j = 1; (1 << j) <= n; j++)
+			for (int i = 1; i + (1 << j) - 1 <= n; i++)
+				st[i][j] = min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
+}
