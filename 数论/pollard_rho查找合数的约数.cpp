ad1e9905eb63832027e9cfc1d2b32fd2802ef0e8
firstruggle <34533968+firstruggle@users.noreply.github.com>
2019-11-11 00:21:02 +0800

    Update pollard_rho查找合数的约数.cpp

--- 数论/pollard_rho查找合数的约数.cpp
+++ 数论/pollard_rho查找合数的约数.cpp
@@ -1,35 +1,68 @@
-//期望复杂度为：根号根号n
-long long pollard_rho(long long p, long long c)
-{
-	long long i=1 , k=2 , x=random(p-1)+1/*随机1~p的数*/;
-	long long y=x , d;
+typedef long long ll;
+map<ll, int>m;
 
-	while (true)
-	{
-		i++;
-		x = (q_mul(x,x,p) + c) % p;//快速乘
-		d = gcd(y-x , p);
-		if (d > 1 && d < p)//找到
-			return d;
-		if (y == x)//循环了没找到
-			return p;
-		if (i == k)//优化功能
-			y = x,k <<= 1;
-	}
+ll mul(ll a, ll b, ll mod) {
+    ll ans = 0;
+    a %= mod;
+    while(b) {
+        if(b & 1) ans = (ans + a) % m;
+        b /= 2;
+        a = (a + a) % m;
+    }
+    return ans;
 }
 
-void find(long long n, long long c)//c为任意写的一个常数
-{
-	if (n == 1)	return;
-	if (n是素数)
-	{
-		insert(n);/*视需求写*/
-		return；
-	}
+ll gcd(ll a, ll b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
 
-	long long p = n;
-	while (p >= n)//直到随机到它的因数
-		p = pollard_rho(p,c--);
-	find(p,c);
-	find(n/p,c);
+ll pollard_rho(ll n, ll c) {//找到n的一个因子
+    ll x = rand() % (n - 2) + 1;
+    ll y = x, i = 1, k = 2;
+    while(1) {
+        i++;
+        x = (mul(x, x, n) + c) + n;//不断调整x2
+        ll d = gcd(y - x, n);
+        if(1 < d && d < n)
+            return d;//找到因子
+        if(y == x)
+            return n;//找到循环，返回n，重新来
+        if(i == k) {//一个优化
+            y = x;
+            k <<= 1;
+        }
+    }
+}
+
+void Find(ll n, ll c) {
+    if(n == 1) return;//递归出口
+
+    if(isprime(n)) {//如果是素数，就加入
+        m[n]++;
+        return;
+    }
+
+    ll p = n;
+    while(p >= n)
+        p = pollard_rho(p, c--);//不断找因子，知道找到为止，返回n说明没找到
+
+    Find(p, c);
+    Find(n / p, c);
+}
+
+int main() {
+    ll n;
+    srand((unsigned)time(NULL));
+    while(cin >> n) {
+        m.clear();
+        Find(n, rand() % (n - 1) + 1);//这是自己设置的一个数
+        cout<<n<<" = ";
+        for(map<ll ,int>::iterator it = m.begin(); it != m.end(); ) {
+            cout << it->first << " ^ " << it->second;
+            if((++it) != m.end())
+               cout << " * ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
